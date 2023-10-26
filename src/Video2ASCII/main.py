d3e883af8ea011934a4eee123aef00851805c0b1
HokageM <m.trzaska663@gmail.com>
2023-10-26 15:22:56 +0200

    add option for only playing mode

--- src/Video2ASCII/main.py
+++ src/Video2ASCII/main.py
@@ -28,21 +28,27 @@ def parse_args(args):
         action="version",
         version=f"Test {__version__}",
     )
-    parser.add_argument('video', metavar='FILE', type=str, help='mp4 that should be transformed')
-    parser.add_argument('-w', '--width', type=int, default=100, help='defines the width of the ASCII art')
+    parser.add_argument('video', metavar='FILE', type=str, help='MP4 that should be transformed.')
+    parser.add_argument('-w', '--width', type=int, default=250,
+                        help='Defines the width of the ASCII art. Default value is set to 250.')
+    parser.add_argument('-p', '--play', type=str, default='',  help='Enter name of previously created '
+                                                                    'ASCII art frames, then it plays the animation.')
     return parser.parse_args(args)
 
 
 def main(args):
     args = parse_args(args)
+    if not args.play:
+        video_processor = VideoProcessor(args.video)
+        video_processor.extract_frames_from_video()
 
-    video_processor = VideoProcessor(args.video)
-    video_processor.extract_frames_from_video()
+        transformer = ASCIITransformer(args.width)
+        transformer.convert_directory_to_ascii(video_processor.get_frame_dir())
 
-    transformer = ASCIITransformer(args.width)
-    transformer.convert_directory_to_ascii(video_processor.get_frame_dir())
-
-    with Animator(transformer.get_ascii_dir()) as animator:
+        play_dir = transformer.get_ascii_dir()
+    else:
+        play_dir = args.play
+    with Animator(play_dir) as animator:
         animator.run_ascii_animation()
 
 
